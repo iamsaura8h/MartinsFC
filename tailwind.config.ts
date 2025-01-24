f010b799e94240108a85371b238b9ebbc7916bd2
iamsaura8h <saurabhchaudhary.net@gmail.com>
2025-01-24 21:53:57 +0530

    project set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#FF0000', // Red for highlights
+        secondary: '#FFFFFF', // White for text and accents
+        background: '#000000', // Black for the main background
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
